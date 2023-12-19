814b5a974646ebc24fda7290964a328f74047dfa
MajesticFish420 <89716887+MajesticFish420@users.noreply.github.com>
2023-12-19 11:45:52 +1100

    Allows slower movement
    Just the first version that I managed to make with slower than 1 movement speed for the box

--- DVD corner bounce animation.py
+++ DVD corner bounce animation.py
@@ -1,23 +1,31 @@
 import pygame
+import random
 
 pygame.init()
-
 screen_width = 800
 screen_height = 600
-
 screen = pygame.display.set_mode((screen_width, screen_height))
+player = pygame.Rect((375, 275, 50, 50))
 
-player = pygame.Rect((300, 250, 50, 50))
+horiMovement = random.randint(-1000, 1000)
+vertMovement = 1000 - abs(horiMovement)
+horiMovement, vertMovement = horiMovement/1000, vertMovement/1000
+horiPos = player[0] + horiMovement
 
 run = True
 while run:
 
     screen.fill((0, 0, 0))
-
     pygame.draw.rect(screen, (255, 0, 0), player)
 
+    horiPos = horiPos + horiMovement
+    player[0] = round(horiPos)
+    print(player[0])
+    print(horiPos)
+
+    """
+    # Movement
     key = pygame.key.get_pressed()
-    
     if key[pygame.K_a] == True:
         player.move_ip(-1, 0)
     if key[pygame.K_d] == True:
@@ -26,7 +34,9 @@ while run:
         player.move_ip(0, -1)
     if key[pygame.K_s] == True:
         player.move_ip(0, 1)
-        
+    """
+
+    # Border Wall 
     if player.left < 0:
         player.left = 0
     elif player.right > screen_width:
