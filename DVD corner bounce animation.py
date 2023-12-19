7803d693b09a529b5fc98fcf1db7ab915a9ea5b8
MajesticFish420 <89716887+MajesticFish420@users.noreply.github.com>
2023-12-19 12:36:17 +1100

    First working version

--- DVD corner bounce animation.py
+++ DVD corner bounce animation.py
@@ -9,8 +9,14 @@ player = pygame.Rect((375, 275, 50, 50))
 
 horiMovement = random.randint(-1000, 1000)
 vertMovement = 1000 - abs(horiMovement)
-horiMovement, vertMovement = horiMovement/1000, vertMovement/1000
+vertMovement = random.choice((vertMovement, 0-vertMovement))
+
+#/1000 to make the movement smaller than 1. /2 to adjust the speed a bit
+horiMovement, vertMovement = (horiMovement/1000)/2, (vertMovement/1000)/2
 horiPos = player[0] + horiMovement
+vertPos = player[1] + vertMovement
+
+corner = 0
 
 run = True
 while run:
@@ -19,9 +25,39 @@ while run:
     pygame.draw.rect(screen, (255, 0, 0), player)
 
     horiPos = horiPos + horiMovement
+    vertPos = vertPos + vertMovement
     player[0] = round(horiPos)
-    print(player[0])
-    print(horiPos)
+    player[1] = round(vertPos)
+
+    # Check for corners
+    """
+    if player.left < 10 and player.top < 10:
+        corner += 1
+        print(corner)
+    elif player.left < 10 and player.bottom > screen_width - 10:
+        corner += 1
+        print(corner)
+    elif player.right > screen_width - 10 and player.top < 10:
+        corner += 1
+        print(corner)
+    elif player.right > screen_width - 10 and player.bottom > screen_width - 10:
+        corner +=1
+        print(corner)
+    """
+
+    # Border Wall
+    if player.left < 0:
+        player.left = 0
+        horiMovement = 0-horiMovement
+    elif player.right > screen_width:
+        player.right = screen_width
+        horiMovement = 0-horiMovement
+    if player.top < 0:
+        player.top = 0
+        vertMovement = 0-vertMovement
+    elif player.bottom > screen_height:
+        player.bottom = screen_height
+        vertMovement = 0-vertMovement
 
     """
     # Movement
@@ -35,16 +71,6 @@ while run:
     if key[pygame.K_s] == True:
         player.move_ip(0, 1)
     """
-
-    # Border Wall 
-    if player.left < 0:
-        player.left = 0
-    elif player.right > screen_width:
-        player.right = screen_width
-    if player.top < 0:
-        player.top = 0
-    elif player.bottom > screen_height:
-        player.bottom = screen_height
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
