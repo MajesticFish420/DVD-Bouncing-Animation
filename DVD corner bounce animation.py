75df3ac806db3653e8b78a6ee7b6d50082a1917e
MajesticFish420 <89716887+MajesticFish420@users.noreply.github.com>
2023-12-20 09:45:16 +1100

    Fully working version

--- DVD corner bounce animation.py
+++ DVD corner bounce animation.py
@@ -1,30 +1,41 @@
 import pygame
 import random
 
+def changeColour(logo, currentColour):
+    logoColours = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    logo = pygame.PixelArray(logo)
+    newColour = random.choice([i for i in logoColours if i != currentColour])
+    logo.replace(currentColour, newColour)
+    currentColour = newColour
+    logo = logo.make_surface()
+    return logo, currentColour
+    
 pygame.init()
-screen_width = 800
-screen_height = 600
+screen_width = 1000
+screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
-player = pygame.Rect((375, 275, 200, 100))
-logo = pygame.image.load("DVD_logo.jpg")
+
+player = pygame.Rect((375, 275, 200, 90))
+
+logo = pygame.image.load("C:/Users/Alasdair/Desktop/Coding/Projects for jobs/DVD corner bounce animation/DVD_logo.jpg")
 logo = pygame.transform.scale(logo, (200, 100))
+currentColour = (0, 0, 0)
+logo, currentColour = changeColour(logo, currentColour)
 
 pygame.display.set_caption("DVD logo bounce")
 
-horiMovement = random.choice((0.5, -0.5))
-vertMovement = random.choice((0.5, -0.5))
+horiMovement = random.choice((0.5, -0.5))/2
+vertMovement = random.choice((0.5, -0.5))/2
 
-#/1000 to make the movement smaller than 1. /2 to adjust the speed a bit
-horiMovement, vertMovement = horiMovement/3, vertMovement/3
 horiPos = player[0] + horiMovement
 vertPos = player[1] + vertMovement
-
 
 run = True
 while run:
 
-    screen.fill((255, 255, 255))
-    pygame.draw.rect(screen, (255, 255, 255), player)
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, (0, 0, 0), player)
     screen.blit(logo, (horiPos, vertPos))
 
     horiPos = horiPos + horiMovement
@@ -33,18 +44,29 @@ while run:
     player[1] = round(vertPos)
 
     # Border Wall
+    # Left Side
     if player.left < 0:
-        player.left = 0
+        player.left = 1
         horiMovement = 0-horiMovement
+        logo, currentColour = changeColour(logo, currentColour)
+
+    # Right Side
     elif player.right > screen_width:
-        player.right = screen_width
+        player.right = screen_width - 1
         horiMovement = 0-horiMovement
+        logo, currentColour = changeColour(logo, currentColour)
+
+    # Top side
     if player.top < 0:
-        player.top = 0
+        player.top = 2
         vertMovement = 0-vertMovement
+        logo, currentColour = changeColour(logo, currentColour)
+
+    # Bottom side
     elif player.bottom > screen_height:
-        player.bottom = screen_height
+        player.bottom = screen_height - 2
         vertMovement = 0-vertMovement
+        logo, currentColour = changeColour(logo, currentColour)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
